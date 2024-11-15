fb44f49773bdbee353c8f3ee628d5039734c8f10
MixDream <max_juldashev@mail.ru>
2024-11-15 19:35:25 +0300

    Update dz.py

--- dz.py
+++ dz.py
@@ -1,37 +1,39 @@
-#.venv\Scripts\activate активация окружения
-# uvicorn dz:app --reload запуск фаст апи
+# .venv\Scripts\activate активация окружения
+# uvicorn main:app --reload запуск фаст апи
 from fastapi import FastAPI, HTTPException, Path
-
 from typing import Dict, Any, Tuple
+from pydantic import BaseModel, validator, field_validator
 
 app = FastAPI()
 
 users: Dict[str, str] = {'1': 'Имя: Example, возраст: 18'}
 
+class User(BaseModel):
+    username: str
+    age: int
+
+    @field_validator('age')
+    def age_must_be_positive(cls, value):
+        if value < 0:
+            raise ValueError("Возраст не может быть отрицательным")
+        return value
 
 @app.get("/users", response_model=Dict[str, str])
 async def get_users() -> Dict[str, str]:
     return users
 
-
-@app.post("/user/{username}/{age}")
-async def create_user(username: str, age: int) -> str:
-    if age < 0:
-        raise HTTPException(status_code=400, detail="Возраст не может быть отрицательным")
+@app.post("/user")
+async def create_user(user: User) -> str:
     new_user_id = str(max(map(int, users.keys())) + 1) if users else '1'
-    users[new_user_id] = f"Имя: {username}, возраст: {age}"
-    return f"User  {new_user_id} is registered"
+    users[new_user_id] = f"Имя: {user.username}, возраст: {user.age}"
+    return f"User {new_user_id} is registered"
 
-
-@app.put("/user/{user_id}/{username}/{age}")
-async def update_user(user_id: str, username: str, age: int) -> str:
+@app.put("/user/{user_id}")
+async def update_user(user_id: str, user: User) -> str:
     if user_id not in users:
         raise HTTPException(status_code=404, detail="Пользователь не найден")
-    if age < 0:
-        raise HTTPException(status_code=400, detail="Возраст не может быть отрицательным")
-    users[user_id] = f"Имя: {username}, возраст: {age}"
-    return f"User  {user_id} has been updated"
-
+    users[user_id] = f"Имя: {user.username}, возраст: {user.age}"
+    return f"User {user_id} has been updated"
 
 @app.delete("/user/{user_id}")
 async def delete_user(user_id: str) -> str:
@@ -39,4 +41,4 @@ async def delete_user(user_id: str) -> str:
         raise HTTPException(status_code=404, detail="Пользователь не найден")
 
     del users[user_id]
-    return f"User  {user_id} has been deleted"
+    return f"User {user_id} has been deleted"
